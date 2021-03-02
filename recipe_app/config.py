3a3669bdf4b783f5ac6ac7e6970927d5f8b5e146
Shaun Norton <shaunnorton87@gmail.com>
2021-03-02 10:27:57 -0800

    changed db

--- recipe_app/config.py
+++ recipe_app/config.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
